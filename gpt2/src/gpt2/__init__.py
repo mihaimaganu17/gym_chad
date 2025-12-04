cdd83c17d98499f18424e68659a17c2b898b6041
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-12-04 12:27:28 +0100

    showcasing gpt2 layers

--- gpt2/src/gpt2/__init__.py
+++ gpt2/src/gpt2/__init__.py
@@ -1,2 +1,14 @@
 def hello() -> str:
+    gpt2_showcase()
     return "Hello from gpt2!"
+
+
+
+def gpt2_showcase():
+    from transformers import GPT2LMHeadModel
+
+    model_hf = GPT2LMHeadModel.from_pretrained("gpt2")
+    gpt2_state_dict = model_hf.state_dict()
+
+    for k, v in gpt2_state_dict.items():
+        print(k, v.shape)
---
+++ gpt2/src/gpt2/train.py
@@ -0,0 +1,4 @@
+class GPTConfig:
+    """Configuration parameters for GPT2 layers"""
+
+
