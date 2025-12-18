97075675f756068acb88e5a7bf5c0c80140477a2
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-12-18 18:07:13 +0100

    Added tiktoken encodings for gpt2 and starter tokens

--- gpt2/src/gpt2/__init__.py
+++ gpt2/src/gpt2/__init__.py
@@ -1,9 +1,32 @@
+import torch
+
 from gpt2.train import GPT
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+if device == torch.device('cpu'):
+    device = torch.device("mps") if torch.backends.mps.is_available() else torch.device('cpu')
 
 def hello() -> str:
     gpt2_showcase()
+
+    num_return_sequences = 5
+    max_new_tokens=30
+
     model = GPT.from_pretrained('gpt2')
-    print("didn't crash")
+    model.eval()
+    model.to(device)
+
+    # load the tokens from the tokeniser
+    import tiktoken
+    # Get the token encodings
+    enc = tiktoken.get_encoding('gpt2')
+    # Encode the beginning prompt
+    tokens = enc.encode("Hello, I'm a language model")
+    # Add the encodings to a tensor
+    tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
+    # Duplicated it 5 times (maybe torch.stack could also work? or torch.cat)
+    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (1, 8) -> (5, 8)
+    x = tokens.to(device)
     
     # Sampling from the HF GPT2
     #for example in gpt2_sample():
